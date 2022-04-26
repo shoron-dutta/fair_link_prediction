ecbae14a947e1c45b1c3a89e28057160d0b8722a
shoron-dutta <duttas@DESKTOP-4OMQ0IS.localdomain>
2022-04-26 03:32:53 -0400

    added argparse

--- fair_link_pred.py
+++ fair_link_pred.py
@@ -95,147 +95,163 @@ from fairlearn.metrics import (
     equalized_odds_difference,
 )
 
+
+
 from torch_geometric.datasets import Planetoid
-dataset = "cora" #"citeseer" #"cora" "pubmed"
-path = osp.join(osp.dirname(osp.realpath('__file__')), "..", "data", dataset)
-dataset = Planetoid(path, dataset, transform=T.NormalizeFeatures())
-
-#test_seeds = [0,1,2,3,4,5]
-test_seeds = [0]
-acc_auc = []
-fairness = []
-
 from tqdm import tqdm
 import numpy as np
-delta = 0.16 # when set to 0, it applies no fairness constraint
-alpha = 0.5
-reg_lambda = 0.5
-for random_seed in test_seeds:
 
-    np.random.seed(random_seed)
-    data = dataset[0]
-    protected_attribute = data.y
-    data.train_mask = data.val_mask = data.test_mask = data.y = None
-    data = train_test_split_edges(data, val_ratio=0.1, test_ratio=0.2)
-    data = data.to(device)
+if __name__ == '__main__':
 
-    num_classes = len(np.unique(protected_attribute))
-    N = data.num_nodes
-    
-    
-    epochs = 1
-    model = GCN(data.num_features, 128).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
-    
+    device = 'cpu'
+    # print(f'Using {device} device')
 
-    Y = torch.LongTensor(protected_attribute).to(device)
-    print(f'Y.shape: {Y.shape}')
-    Y_aux = (
-        Y[data.train_pos_edge_index[0, :]] != Y[data.train_pos_edge_index[1, :]]
-    ).to(device)
-    randomization = (
-        torch.FloatTensor(epochs, Y_aux.size(0)).uniform_() < 0.5 + delta
-    ).to(device)
-    
-    
-    best_val_perf = test_perf = 0
-    for epoch in tqdm(range(1, epochs)):
+    parser = argparse.ArgumentParser(description='Fair link prediction using regularized mutual information')
+    parser.add_argument('--dataset', type=str, default='cora', help='name of the dataset: citeseer, pubmed, cora')
+    parser.add_argument('--num_epochs', type=int, default=2, help='number of epochs')
+    parser.add_argument('--reg_lambda', type=float, default=0.7, help='regularization parameter')
+    parser.add_argument('--alpha', type=float, default=0.5, help='alpha parameter for entropy')
+    args = parser.parse_args()
+
+    dataset = "cora" #"citeseer" #"cora" "pubmed"
+    path = osp.join(osp.dirname(osp.realpath('__file__')), "..", "data", dataset)
+    dataset = Planetoid(path, dataset, transform=T.NormalizeFeatures())
+
+    #test_seeds = [0,1,2,3,4,5]
+    test_seeds = [0]
+    acc_auc = []
+    fairness = []
+
+
+    delta = 0.16 # when set to 0, it applies no fairness constraint
+    alpha = args.alpha
+    reg_lambda = args.reg_lambda
+    for random_seed in test_seeds:
+
+        np.random.seed(random_seed)
+        data = dataset[0]
+        protected_attribute = data.y
+        data.train_mask = data.val_mask = data.test_mask = data.y = None
+        data = train_test_split_edges(data, val_ratio=0.1, test_ratio=0.2)
+        data = data.to(device)
+
+        num_classes = len(np.unique(protected_attribute))
+        N = data.num_nodes
         
-        # TRAINING    
-        neg_edges_tr = negative_sampling(
-            edge_index=data.train_pos_edge_index,
-            num_nodes=N,
-            num_neg_samples=data.train_pos_edge_index.size(1) // 2,
+        
+        epochs = args.num_epochs
+        model = GCN(data.num_features, 128).to(device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+        
+
+        Y = torch.LongTensor(protected_attribute).to(device)
+        print(f'Y.shape: {Y.shape}')
+        Y_aux = (
+            Y[data.train_pos_edge_index[0, :]] != Y[data.train_pos_edge_index[1, :]]
         ).to(device)
+        randomization = (
+            torch.FloatTensor(epochs, Y_aux.size(0)).uniform_() < 0.5 + delta
+        ).to(device)
+        
+        
+        best_val_perf = test_perf = 0
+        for epoch in tqdm(range(1, epochs)):
+            
+            # TRAINING    
+            neg_edges_tr = negative_sampling(
+                edge_index=data.train_pos_edge_index,
+                num_nodes=N,
+                num_neg_samples=data.train_pos_edge_index.size(1) // 2,
+            ).to(device)
 
-        if epoch == 1 or epoch % 10 == 0:
-            keep = torch.where(randomization[epoch], Y_aux, ~Y_aux)
+            if epoch == 1 or epoch % 10 == 0:
+                keep = torch.where(randomization[epoch], Y_aux, ~Y_aux)
 
-        model.train()
-        optimizer.zero_grad()
+            model.train()
+            optimizer.zero_grad()
 
-        z = model.encode(data.x, data.train_pos_edge_index[:, keep])
-        link_logits, _ = model.decode(
-            z, data.train_pos_edge_index[:, keep], neg_edges_tr
+            z = model.encode(data.x, data.train_pos_edge_index[:, keep])
+            link_logits, _ = model.decode(
+                z, data.train_pos_edge_index[:, keep], neg_edges_tr
+            )
+            tr_labels = get_link_labels(
+                data.train_pos_edge_index[:, keep], neg_edges_tr
+            ).to(device)
+            
+            # mutual info
+            logit_arr = link_logits.detach().numpy()
+            print(f'logit_arr.shape: {logit_arr.shape}')
+            labels_arr = tr_labels.detach().numpy()
+            # TODO : take meaningful mutual information
+            k1 = get_kernel_mat(logit_arr, logit_arr)
+            k2 = get_kernel_mat(Y, Y)
+            #print("Computed kernel mat")
+            nk1 = get_normalized_kernel_mat(k1)
+            nk2 = get_normalized_kernel_mat(k2)
+            
+            #print("Computed normalized kernel mat")
+            mutual_info = get_mutual_info(nk1, nk2, alpha)
+            #print("Computed mutual info")
+
+
+            loss = F.binary_cross_entropy_with_logits(link_logits, tr_labels) + reg_lambda * mutual_info
+            loss.backward()
+            optimizer.step()
+
+            # EVALUATION
+            model.eval()
+            perfs = []
+            for prefix in ["val", "test"]:
+                pos_edge_index = data[f"{prefix}_pos_edge_index"]
+                neg_edge_index = data[f"{prefix}_neg_edge_index"]
+                with torch.no_grad():
+                    z = model.encode(data.x, data.train_pos_edge_index)
+                    link_logits, edge_idx = model.decode(z, pos_edge_index, neg_edge_index)
+                link_probs = link_logits.sigmoid()
+                link_labels = get_link_labels(pos_edge_index, neg_edge_index)
+                auc = roc_auc_score(link_labels.cpu(), link_probs.cpu())
+                perfs.append(auc)
+
+            val_perf, tmp_test_perf = perfs
+            if val_perf > best_val_perf:
+                best_val_perf = val_perf
+                test_perf = tmp_test_perf
+            if epoch%10==0:
+                log = "Epoch: {:03d}, Loss: {:.4f}, Val: {:.4f}, Test: {:.4f}"
+                print(log.format(epoch, loss, best_val_perf, test_perf))
+
+        # FAIRNESS
+        auc = test_perf
+        cut = [0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75]
+        best_acc = 0
+        best_cut = 0.5
+        for i in cut:
+            
+            acc = accuracy_score(link_labels.cpu(), link_probs.cpu() >= i)
+            if acc > best_acc:
+                best_acc = acc
+                best_cut = i
+        f = prediction_fairness(
+            edge_idx.cpu(), link_labels.cpu(), link_probs.cpu() >= best_cut, Y.cpu()
         )
-        tr_labels = get_link_labels(
-            data.train_pos_edge_index[:, keep], neg_edges_tr
-        ).to(device)
-        
-        # mutual info
-        logit_arr = link_logits.detach().numpy()
-        print(f'logit_arr.shape: {logit_arr.shape}')
-        labels_arr = tr_labels.detach().numpy()
-         # TODO : take meaningful mutual information
-        k1 = get_kernel_mat(logit_arr, logit_arr)
-        k2 = get_kernel_mat(Y, Y)
-        #print("Computed kernel mat")
-        nk1 = get_normalized_kernel_mat(k1)
-        nk2 = get_normalized_kernel_mat(k2)
-        
-        #print("Computed normalized kernel mat")
-        mutual_info = get_mutual_info(nk1, nk2, alpha)
-        #print("Computed mutual info")
-
-
-        loss = F.binary_cross_entropy_with_logits(link_logits, tr_labels) + reg_lambda * mutual_info
-        loss.backward()
-        optimizer.step()
-
-        # EVALUATION
-        model.eval()
-        perfs = []
-        for prefix in ["val", "test"]:
-            pos_edge_index = data[f"{prefix}_pos_edge_index"]
-            neg_edge_index = data[f"{prefix}_neg_edge_index"]
-            with torch.no_grad():
-                z = model.encode(data.x, data.train_pos_edge_index)
-                link_logits, edge_idx = model.decode(z, pos_edge_index, neg_edge_index)
-            link_probs = link_logits.sigmoid()
-            link_labels = get_link_labels(pos_edge_index, neg_edge_index)
-            auc = roc_auc_score(link_labels.cpu(), link_probs.cpu())
-            perfs.append(auc)
-
-        val_perf, tmp_test_perf = perfs
-        if val_perf > best_val_perf:
-            best_val_perf = val_perf
-            test_perf = tmp_test_perf
-        if epoch%10==0:
-            log = "Epoch: {:03d}, Loss: {:.4f}, Val: {:.4f}, Test: {:.4f}"
-            print(log.format(epoch, loss, best_val_perf, test_perf))
-
-    # FAIRNESS
-    auc = test_perf
-    cut = [0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75]
-    best_acc = 0
-    best_cut = 0.5
-    for i in cut:
-        
-        acc = accuracy_score(link_labels.cpu(), link_probs.cpu() >= i)
-        if acc > best_acc:
-            best_acc = acc
-            best_cut = i
-    f = prediction_fairness(
-        edge_idx.cpu(), link_labels.cpu(), link_probs.cpu() >= best_cut, Y.cpu()
-    )
-    print(f'best cut = {best_cut}')
-    acc_auc.append([best_acc * 100, auc * 100])
-    fairness.append([x * 100 for x in f])
+        print(f'best cut = {best_cut}')
+        acc_auc.append([best_acc * 100, auc * 100])
+        fairness.append([x * 100 for x in f])
 
 
 
-ma = np.mean(np.asarray(acc_auc), axis=0)
-mf = np.mean(np.asarray(fairness), axis=0)
+    ma = np.mean(np.asarray(acc_auc), axis=0)
+    mf = np.mean(np.asarray(fairness), axis=0)
 
-sa = np.std(np.asarray(acc_auc), axis=0)
-sf = np.std(np.asarray(fairness), axis=0)
+    sa = np.std(np.asarray(acc_auc), axis=0)
+    sf = np.std(np.asarray(fairness), axis=0)
 
-print(f"ACC: {ma[0]:2f} +- {sa[0]:2f}")
-print(f"AUC: {ma[1]:2f} +- {sa[1]:2f}")
+    print(f"ACC: {ma[0]:2f} +- {sa[0]:2f}")
+    print(f"AUC: {ma[1]:2f} +- {sa[1]:2f}")
 
-print(f"DP mix: {mf[0]:2f} +- {sf[0]:2f}")
-print(f"EoP mix: {mf[1]:2f} +- {sf[1]:2f}")
-print(f"DP group: {mf[2]:2f} +- {sf[2]:2f}")
-print(f"EoP group: {mf[3]:2f} +- {sf[3]:2f}")
-print(f"DP sub: {mf[4]:2f} +- {sf[4]:2f}")
-print(f"EoP sub: {mf[5]:2f} +- {sf[5]:2f}")
+    print(f"DP mix: {mf[0]:2f} +- {sf[0]:2f}")
+    print(f"EoP mix: {mf[1]:2f} +- {sf[1]:2f}")
+    print(f"DP group: {mf[2]:2f} +- {sf[2]:2f}")
+    print(f"EoP group: {mf[3]:2f} +- {sf[3]:2f}")
+    print(f"DP sub: {mf[4]:2f} +- {sf[4]:2f}")
+    print(f"EoP sub: {mf[5]:2f} +- {sf[5]:2f}")
